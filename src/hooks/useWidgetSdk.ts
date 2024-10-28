a41426c81d63aba22c33750cd50b8d21b9fe253d
rahimlisarkhan <rahimlisarkhan@gmail.com>
2024-10-29 00:28:38 +0400

    updatE

--- src/hooks/useWidgetSdk.ts
+++ src/hooks/useWidgetSdk.ts
@@ -3,39 +3,30 @@ import { useEffect } from "react";
 
 export const useWidgetSdk = () => {
   useEffect(() => {
-    console.log(window);
-
     // Check to ensure the code only runs on the client side
     if (typeof window !== "undefined") {
-      // Prevent multiple script insertions
-      if (
-        !document.querySelector(
-          "script[src='https://widget.getwhelp.com/app.js']"
-        )
-      ) {
-        const libraryEl = document.createElement("script");
-        libraryEl.src = "https://widget.getwhelp.com/app.js";
+      const libraryEl = document.createElement("script");
+      libraryEl.src = "https://widget.getwhelp.com/app.js";
 
-        libraryEl.onload = () => {
-          const initEl = document.createElement("script");
-          initEl.text = `
+      libraryEl.onload = () => {
+        const initEl = document.createElement("script");
+        initEl.text = `
             Whelp("init", {
               app_id: "ddaa4869af1be6a5e5acfa2a8271692d",
               viewportSetupOff:true
             });
           `;
-          document.body.appendChild(initEl);
-        };
+        document.body.appendChild(initEl);
+      };
 
-        document.body.appendChild(libraryEl);
+      document.body.appendChild(libraryEl);
 
-        // Cleanup function to remove scripts on unmount
-        return () => {
-          document.body.removeChild(libraryEl);
-          const initEl = document.querySelector("script[data-widget-init]");
-          if (initEl) document.body.removeChild(initEl);
-        };
-      }
+      // Cleanup function to remove scripts on unmount
+      return () => {
+        document.body.removeChild(libraryEl);
+        const initEl = document.querySelector("script[data-widget-init]");
+        if (initEl) document.body.removeChild(initEl);
+      };
     }
   }, []);
 };
